ea518828fb568bb455c33d696048d88e95f8e82b
sully martinez <sullymartinezj@gmail.com>
2019-04-01 17:28:58 -0400

    updated file adding ejs

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-  'url' : 'mongodb+srv://sully:demo@cluster0-5bzvm.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  'url' : 'mongodb+srv://sully:demo@cluster0-4l3nf.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
   'dbName': 'demo'
 };
